096bb9f4d1176e192cff2028bfee2f02fb618a00
Aylar Soltani <Aylar+SAN-analytics@users.noreply.github.com>
2022-03-22 14:02:49 +0100

    countriesAPI-react-typescript

---
+++ src/redux/reducers/index.ts
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux'
+import counter from '../reducers/counter'
+
+import country from './country'
+
+const createRootReducer = () =>
+  combineReducers({
+    country,
+    counter,
+  })
+
+export default createRootReducer
